39d4cda011b9d0161f7d6a26cf04e34a4298d7dc
ktk-n <katsuki.diesel@gmail.com>
2021-04-18 18:55:09 +0900

    TODOリスト

--- vue-todo-list/src/App.vue
+++ vue-todo-list/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <!-- ToDoListを使用する -->
+  <ToDoList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // ToDoListを<template>で使用できるようにする
+    ToDoList
   }
 }
 </script>
